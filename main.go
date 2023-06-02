b461c8841c796bd0ee2e4b09ec6112b3ab8f019f
krish palaniappan <krish@getsnowpal.com>
2023-06-02 15:51:05 -0400

    doc: updated documentation

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
+	"github.com/snowpal/pitch-building-blocks-sdk/lib/config"
 	"github.com/snowpal/pitch-building-blocks-sdk/lib/recipes"
 )
 
 func main() {
-	recipes.RegisterFewUsers()
-	recipes.CreatePrivateConversation()
+	var err error
+	if config.Files, err = config.InitConfigFiles(); err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	recipeID := 1
+	switch recipeID {
+	case 1:
+		log.Info("Run Recipe1")
+		recipes.RegisterFewUsers()
+		break
+	case 2:
+		log.Info("Run Recipe2")
+		recipes.CreatePrivateConversation()
+		break
+	default:
+		log.Info("pick a specific recipe to run")
+	}
 }
